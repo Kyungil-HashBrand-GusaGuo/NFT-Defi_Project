0b62d0b8755851cc6ce0e63c0f4ecd601e7abe85
taehhh8 <taehyun8534@gmail.com>
2022-06-03 12:32:16 +0900

    6/3  Header 구현
    6/3  Header 구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { MainPage } from './pages';
+import { Navbar } from './components'
+// import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <Routes>
         <Route path='/' element={<MainPage/>} />
       </Routes>
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react'
+import './Navbar.css';
+import { MdReorder } from "react-icons/md";
+import {HeadImg} from '../images'
+
+function Navbar() {
+
+    const [showLinks, setShowLinks] = useState(false); 
+
+    return (
+        <div className='Navbar'>
+            <div className='leftSide'>
+                <a href='/'><img className='headimg' src={HeadImg}></img></a>
+            </div>
+            <div className='rightSide'>
+                <div className='links' id={showLinks ? "hidden" : ""}>
+                    <a href='/pre-minting'>민팅페이지</a>
+                    <a href='/whitepaper'>NFT조회</a>
+                    <a href='/'>관리자페이지</a>
+                    <button>Connect Wallet</button>
+                </div>
+                    {" "}
+                    <MdReorder className='listicon' size={40} onClick={()=>setShowLinks(!showLinks)}/>
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
