1ad5f764d93f770d8769c7b3dc38b6765d810b63
jaewoneeee <chou60400@naver.com>
2022-06-04 21:46:02 +0900

    FEAT : connect wallet (redux code)

--- frontend/src/components/global/Navbar.js
+++ frontend/src/components/global/Navbar.js
@@ -1,11 +1,26 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import './Navbar.css';
 import { MdReorder } from "react-icons/md";
 import {HeadImg} from '../../images'
+import { useDispatch, useSelector } from 'react-redux';
+import { connectAccount } from '../../redux/actiion/connectAccount' 
 
 function Navbar() {
 
+    const dispatch = useDispatch();
+
     const [showLinks, setShowLinks] = useState(false); 
+    const {account} = useSelector(state => state.account)
+
+    const connectWallet = () => {
+        if(window.klaytn)
+        dispatch(connectAccount.getAccount())
+        //getAccount();
+    }
+
+    useEffect(() => {
+        if(account === '') connectWallet()
+      },);
 
     return (
         <div className='Navbar'>
@@ -17,7 +32,10 @@ function Navbar() {
                     <a href='/pre-minting'>민팅페이지</a>
                     <a href='/mypage'>마이페이지</a>
                     <a href='/admin'>관리자페이지</a>
-                    <a><button>Connect Wallet</button></a>
+                    {
+                        account === '' ? <a><button onClick={()=>connectWallet()}>Connect Wallet</button></a> 
+                        : <a><button>{account.substr(0,6)}...{account.slice(-6)}</button></a> 
+                    }
                 </div>
                     {/* <button>open</button> */}
                     <MdReorder className='listicon' size={40} onClick={()=>setShowLinks(!showLinks)}/>
