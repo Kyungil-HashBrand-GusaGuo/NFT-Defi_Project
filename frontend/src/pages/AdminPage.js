267278e193407beb11cb06daa35102cf127ba6be
junghyunB <bjh940126@gmail.com>
2022-06-06 00:35:32 +0900

    재원이 브랜치 merge WhiteListIssue 해결

--- frontend/src/pages/AdminPage.js
+++ frontend/src/pages/AdminPage.js
@@ -6,20 +6,18 @@ import { addWhiteListAccount } from '../redux/action/addWhiteListAccount';
 
 const AdminPage = () => {
   const dispatch = useDispatch();
-  //const {account} = useSelector(state => state.account);
-  //const [address, setAddress] = useState();
+  const {account} = useSelector(state => state.account);
+  const [address, setAddress] = useState();
 
-  let account = "0xaC0d580B21118dB9Ea5d752d8950e9C2436575DE";
-  let address = "0xaC0d580B21118dB9Ea5d752d8950e9C2436575DE";
 
 
   const addWhiteList = () => {
-    dispatch(addWhiteListAccount.addWhiteList())
+    dispatch(addWhiteListAccount.addWhiteList(account,address))
   }
 
   const changeAddress = (e) => {
     e.preventDefault();
-    //setAddress(e.target.value);
+    setAddress(e.target.value);
   }
 
   return (
--- frontend/src/redux/action/addWhiteListAccount.js
+++ frontend/src/redux/action/addWhiteListAccount.js
@@ -1,20 +1,13 @@
 import { caver, RandomJolamanContract, MINT_CONTRACT_ADDRESS } from "../../caverConfig";
-//import { useSelector } from 'react-redux';
 
-function addWhiteList() {
-  
-  //let {account} = useSelector(state => state.account);
-  
-  // console.log("해당계정", account)  // 정현이코드에선 작동할듯 계정 가져오기때문에
-  // console.log("추가계정", address)
 
-  let account = "0xaC0d580B21118dB9Ea5d752d8950e9C2436575DE";
-  let address = "0xaC0d580B21118dB9Ea5d752d8950e9C2436575DE";
+function addWhiteList(account, address) {
 
-  return async (dispatch) => {
-    console.log("실행됨?")
+    console.log("해당계정", account)  // 정현이코드에선 작동할듯 계정 가져오기때문에
+    console.log("추가계정", address)
+
+    return async (dispatch) => {
         try {
-          console.log("여긴와?")
             const response = await caver.klay.sendTransaction({
               from: account,
               to: MINT_CONTRACT_ADDRESS,
