e2e72e03a37197942a727ec24670026dc46d0f8d
junghyunB <bjh940126@gmail.com>
2022-06-07 09:23:41 +0900

    수정

--- frontend/src/pages/MyPage.js
+++ frontend/src/pages/MyPage.js
@@ -4,24 +4,6 @@ import { MyCardList } from '../components';
 
 const MyPage = () => {
 
-<<<<<<< HEAD
-  const { account } = useSelector(state => state.account);
-
-
-
-  // let account = "0xaC0d580B21118dB9Ea5d752d8950e9C2436575DE";
-  const [showmint, setShowmint] = useState("");
-  const ownedTokenId = async() => {
-    const response = await RandomJolamanContract.methods.getTotalOwnedTokens(account).call()
-    setShowmint(response);
-    console.log("내민팅",response);
-  }
-
-  useEffect(()=> {
-    ownedTokenId();
-  },[account])
-=======
->>>>>>> 83aba416c34e5633b4c85608912abd5140abf6d8
 
   return (
     <div className='myPageContainer'>
