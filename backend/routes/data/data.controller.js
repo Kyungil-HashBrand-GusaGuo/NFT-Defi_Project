52820dd2ab2b1d2da1c3ff176f027c7e39f486ad
sieun95 <92782143+sieun95@users.noreply.github.com>
2022-06-07 09:52:45 +0900

    FEAT: page 정리

--- backend/routes/data/data.controller.js
+++ backend/routes/data/data.controller.js
@@ -1,10 +1,10 @@
 const pool = require("../../db");
 
-const dbtest = async (req, res) => {
+const test = async (req, res) => {
   const [result] = await pool.query(`SELECT * FROM sub`);
   res.json(result);
 };
 
 module.exports = {
-  dbtest,
+  test,
 };
