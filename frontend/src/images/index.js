78e292784f276af50c1dc3e5bdd737fe1635cfa5
taehhh8 <taehyun8534@gmail.com>
2022-06-08 12:05:20 +0900

    6/8 메인 수정
    6/8 메인 수정

--- frontend/src/images/index.js
+++ frontend/src/images/index.js
@@ -3,6 +3,20 @@ import HeadImg from './headzol.png'
 // import SpImg from './spbg.jpeg'
 import Test from './test1.png'
 import zolmain1 from './zolca10.png'
+import Team1 from './Team1.png'
+import Team2 from './Team2.png'
+import Team3 from './Team3.png'
+import Team4 from './Team4.png'
+import Team5 from './Team5.png'
 
 
-export {HeadImg , Test, zolmain1}
+export {HeadImg,
+    Test,
+    zolmain1,
+    Team1,
+    Team2,
+    Team3,
+    Team4,
+    Team5,
+
+}
