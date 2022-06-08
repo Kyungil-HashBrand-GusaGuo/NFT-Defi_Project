29d637c86739829951dff17ab59f1054a052068b
jaewoneeee <chou60400@naver.com>
2022-06-08 11:43:40 +0900

    CSS : mainpage 'parters' update

--- frontend/src/components/MainPage/Partners.js
+++ frontend/src/components/MainPage/Partners.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { logo_DCcomics, logo_Marvel, logo_Diseny, logo_PolaBear } from '../../images'
 import './Partners.css'
+
 
 const Partners = () => {
   return (
     <div className='partnersContainer'>
-        Partners
+        <div className='headSection'>Partners</div>
+        <div className='logoSection'>
+          <div className='partnerLogo' style={{backgroundImage:"url(" + logo_DCcomics + ")"}}></div>
+          <div className='partnerLogo' style={{backgroundImage:"url(" + logo_Marvel + ")"}}></div>
+          <div className='partnerLogo' style={{backgroundImage:"url(" + logo_Diseny + ")"}}></div>
+          <div className='partnerLogo' style={{backgroundImage:"url(" + logo_PolaBear + ")"}}></div>
+        </div>
     </div>
   )
 }
--- frontend/src/images/index.js
+++ frontend/src/images/index.js
@@ -3,6 +3,9 @@ import HeadImg from './headzol.png'
 // import SpImg from './spbg.jpeg'
 import Test from './test1.png'
 import zolmain1 from './zolca10.png'
+import logo_DCcomics from './logo_DCcomics.png'
+import logo_Diseny from './logo_Diseny.png'
+import logo_Marvel from './logo_Marvel.png'
+import logo_PolaBear from './logo_PolaBear.png'
 
-
-export {HeadImg , Test, zolmain1}
+export {HeadImg , Test, zolmain1, logo_DCcomics, logo_Diseny, logo_Marvel, logo_PolaBear}
