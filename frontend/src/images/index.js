1c67aca1ae49ca225cb8bf1b940071abde3b2142
taehhh8 <taehyun8534@gmail.com>
2022-06-08 12:05:20 +0900

    6/8 메인 수정
    6/8 메인 수정

--- frontend/src/images/index.js
+++ frontend/src/images/index.js
@@ -1,11 +1,29 @@
-import HeadImg from './headzol.png'
+import HeadImg from "./headzol.png";
 // import HeadImg from './zol1.png'
 // import SpImg from './spbg.jpeg'
-import Test from './test1.png'
-import zolmain1 from './zolca10.png'
-import logo_DCcomics from './logo_DCcomics.png'
-import logo_Diseny from './logo_Diseny.png'
-import logo_Marvel from './logo_Marvel.png'
-import logo_PolaBear from './logo_PolaBear.png'
+import Test from "./test1.png";
+import zolmain1 from "./zolca10.png";
+import logo_DCcomics from "./logo_DCcomics.png";
+import logo_Diseny from "./logo_Diseny.png";
+import logo_Marvel from "./logo_Marvel.png";
+import logo_PolaBear from "./logo_PolaBear.png";
+import Team1 from "./Team1.png";
+import Team2 from "./Team2.png";
+import Team3 from "./Team3.png";
+import Team4 from "./Team4.png";
+import Team5 from "./Team5.png";
 
-export {HeadImg , Test, zolmain1, logo_DCcomics, logo_Diseny, logo_Marvel, logo_PolaBear}
+export {
+  HeadImg,
+  Test,
+  zolmain1,
+  Team1,
+  Team2,
+  Team3,
+  Team4,
+  Team5,
+  logo_DCcomics,
+  logo_Diseny,
+  logo_Marvel,
+  logo_PolaBear,
+};
