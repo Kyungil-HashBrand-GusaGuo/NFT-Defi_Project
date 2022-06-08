bb8b8fb6e1055052caca233497c5e32d4931f93d
taehhh8 <taehyun8534@gmail.com>
2022-06-08 10:11:39 +0900

    6/8수정
    6/8수정

--- frontend/src/images/index.js
+++ frontend/src/images/index.js
@@ -1,5 +1,5 @@
-// import HeadImg from './headzol.jpeg'
-import HeadImg from './zol1.png'
+import HeadImg from './headzol.png'
+// import HeadImg from './zol1.png'
 // import SpImg from './spbg.jpeg'
 import Test from './test1.png'
 import zolmain1 from './zolca10.png'
