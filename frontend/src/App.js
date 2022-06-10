4efbcd69721be31921988862fd35af469d1631d6
jaewoneeee <chou60400@naver.com>
2022-06-10 17:25:49 +0900

    FEAT : sellpage useNavigate update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import { MainPage, PreMintingPage, AdminPage, MyPage, AllMintPage } from './pages';
+import { MainPage, PreMintingPage, AdminPage, MyPage, AllMintPage, SellPage } from './pages';
 import { Navbar, Footer } from './components'
 
 
@@ -16,6 +16,7 @@ function App() {
         <Route path='/pre-minting' element={<PreMintingPage/>} />
         <Route path='/admin' element={<AdminPage/>} />
         <Route path='/all-minting' element={<AllMintPage/>} />
+        <Route path='/sellpage/:id' element={<SellPage/>}/>
       </Routes>
     </div>
       <Footer/>
---
+++ frontend/src/pages/SellPage.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+
+const SellPage = () => {
+
+    let {id} = useParams()
+    console.log(id)
+
+  return (
+    <>
+    <div>SellPage</div>
+    <div>SellPage</div>
+    <div>SellPage</div>
+    <div>SellPage</div>
+    <div>SellPage</div>
+    <div>SellPage</div>
+    <div>SellPage</div>
+    <div>SellPage</div>
+    <div>SellPage</div>
+    <div>SellPage</div>
+    </>
+  )
+}
+
+export default SellPage
