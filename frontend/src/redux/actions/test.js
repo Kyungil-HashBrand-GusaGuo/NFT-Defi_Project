c551807acfc5c006a650d0c13b3d0136493fe4d9
jaewoneeee <chou60400@naver.com>
2022-06-10 17:55:26 +0900

    FEAT : sellpage redux update

--- frontend/src/redux/actions/test.js
+++ frontend/src/redux/actions/test.js
@@ -1,16 +1,42 @@
+import {
+    caver,
+    RandomJolamanContract,
+    MINT_CONTRACT_ADDRESS,
+    SALE_CONTRACT_ADDRESS,
+    setDataContract,
+    SaleContract
+  
+  } from "../../caverConfig.js";
+
 
 function testAction(edition, account) {
 
     return async (dispatch) => {
         try {
-            console.log(edition)
-            console.log(account)
-
-
-
-          } catch (error){
-            console.error(error);
-          }
+            const res = await setDataContract.methods.gettypeToId(edition).call()
+            let tokenId = res;
+                const response = await caver.klay.sendTransaction({
+                from: account,
+                to: MINT_CONTRACT_ADDRESS,
+                gas: "3000000",
+                data: RandomJolamanContract.methods.approve(MINT_CONTRACT_ADDRESS, tokenId).encodeABI()   
+                })
+          // 판매 등록 함수 판매 등록시 두번쨰 함수
+                if(response.status) {
+                    let price = 2
+                    const SellNFT = async(edition, price) => {
+                        await caver.klay.sendTransaction({
+                          from: account,
+                          to: SALE_CONTRACT_ADDRESS,
+                          gas: "3000000",
+                          data: SaleContract.methods.SellJolamanToken(edition, price)
+                        })
+                      } 
+                      SellNFT()
+                    }
+                } catch(error) {
+                console.error(error);
+            }
     }
 }
 
