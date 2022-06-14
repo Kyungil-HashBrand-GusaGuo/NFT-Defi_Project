257cf02c03459330e6be03646453751b4fe7e69b
jaewoneeee <chou60400@naver.com>
2022-06-14 17:21:59 +0900

    CSS : buypage & modal css update

--- frontend/src/pages/BuyPage.js
+++ frontend/src/pages/BuyPage.js
@@ -1,19 +1,25 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useSelector } from 'react-redux';
-import './BuyPage.css'
+import klayIcon2 from '../images/klaytn.png';
+//import './BuyPage.css'
 import { BuyModal } from '../components';
+import axios from 'axios';
 
 const BuyPage = () => {
 
     let {edition} = useParams()
     const [sellNftPrice, setSellNftPrice] = useState()
     const [buyModal, setBuyModal] = useState(false)
+    const [showMint, setShowMint] = useState();
     const { account } = useSelector(state => state.account)
     const { sellingAllNftData } = useSelector(state => state.transactionNFT)
+
     
-    const checkSellNftPrice = () => {
-        //console.log(sellingAllNftData)
+    const callBuyNft = async() => {
+        const mintJSON = await axios.get(`https://gateway.pinata.cloud/ipfs/QmaavyzfX6XzVNJx4zKCQVNDJWwQJx9xUC6gmDfddxvQ6p/${edition}.json`)
+        setShowMint(mintJSON.data);
+
         for(let i=0; i < sellingAllNftData.length; i++){
             if(sellingAllNftData[i].id == edition){
                 setSellNftPrice(sellingAllNftData[i].price)
@@ -26,25 +32,113 @@ const BuyPage = () => {
       }
 
     useEffect (() => {
-        checkSellNftPrice()
+        callBuyNft()
     },[])
 
   return (
-    <div className='BuyPageContainer'>
-        <div>BuyPage</div>
-        <div>BuyPage</div>
-        <div>number : {edition}</div>
-        <div>price : {sellNftPrice}</div>
-        <button onClick={changeBuyModalState} >Buy NFT</button>
-        <div>
-        {
-            buyModal ? <BuyModal edition={edition} account={account} /> : null
-        }
-        </div>
-        <div>BuyPage</div>
-        <div>BuyPage</div>
+    <>
+    {
+        buyModal ? <BuyModal edition={edition} account={account} /> : null
+    }
+<div className='sellMainContainer'>
+    <div className='SellMainSection'>
+      <div className='SellTitleContainer'>
+          <h2>List item for sale</h2>
+      </div>
+      { showMint ? 
+      <div className='sellTxtContainer'>
+        <div className='leftSellContainer'>
+          <div className='leftSection'>
+            <div className='leftInputContainer'>
+              <div>
+              </div>
+              <div className='leftTableTitle'>
+                <p>NFT Attributes</p>
+              </div>
+              <div className='leftAttributeContainer'>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>{showMint.attributes[0].trait_type}</th>
+                      <th>{showMint.attributes[1].trait_type}</th>
+                      <th>{showMint.attributes[2].trait_type}</th>
+                      <th>{showMint.attributes[3].trait_type}</th>
+                      <th>{showMint.attributes[4].trait_type}</th>
+                      <th>{showMint.attributes[5].trait_type}</th>
+                      <th>{showMint.attributes[6].trait_type}</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>{showMint.attributes[0].value}</td>
+                      <td>{showMint.attributes[1].value}</td>
+                      <td>{showMint.attributes[2].value}</td>
+                      <td>{showMint.attributes[3].value}</td>
+                      <td>{showMint.attributes[4].value}</td>
+                      <td>{showMint.attributes[5].value}</td>
+                      <td>{showMint.attributes[6].value}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
 
+                    <div className='leftInput' >
+                      <div className='leftInputTitle'>
+                        <p>BUY NFT</p>
+                      </div>
+                    </div>
+                    <div className='leftbtn'>
+                      <button onClick={changeBuyModalState} className="learn-more">Buy Now</button>
+                    </div>
+
+            </div>
+          </div>
+        </div>
+        
+        <div className='rightSellContainer'>
+          <div className='rightCardMainContainer'>
+            
+            <div className='rightCardList'>
+              <div className='rightNftCard'
+                style={{
+                  backgroundImage: 
+                      "url(" + 
+                      `${showMint.image}` + 
+                      ")"
+              }}
+              >
+              </div>
+              <div className='rightCardtxtContainer'>
+                    <div className='rightCardTxt'>
+                      <div className='rightCardListTitle'>
+                          <p>Zolaman nft</p> 
+                      </div>
+                      <div className='rightCardListName'>
+                          <p>{showMint.name}</p>
+                      </div>
+                    </div>
+
+                        <div className='rightCardTxt'>
+                          <div className='rightCardListTitle'>
+                          <p>Price </p>
+                          </div>
+                          <div className='rightCardListPrice'>
+                              <img className='klayicon' src={klayIcon2}/><p>{sellNftPrice}</p>
+                          </div>
+                        </div>
+                      
+              </div>
+            </div>
+            
+          </div>
+        </div>
+       
+      </div>
+      : null  
+      }
     </div>
+  </div>
+  </>
   )
 }
 
