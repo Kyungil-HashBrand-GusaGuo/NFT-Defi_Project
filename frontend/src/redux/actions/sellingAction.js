251e4406aa17061b222e0017cf9e3b122fbbab5b
rmfkxhxh <manboong94@gmail.com>
2022-06-17 15:54:53 +0900

    feat : selling action migrate for ropsten

--- frontend/src/redux/actions/sellingAction.js
+++ frontend/src/redux/actions/sellingAction.js
@@ -1,11 +1,10 @@
 import {
-  caver,
   RandomJolamanContract,
   MINT_CONTRACT_ADDRESS,
   SALE_CONTRACT_ADDRESS,
   setDataContract,
   SaleContract,
-} from "../../caverConfig.js";
+} from "../../ropstenConfig.js";
 
 function sellAction(edition, account, price) {
   // console.log(edition)
@@ -16,31 +15,32 @@ function sellAction(edition, account, price) {
     //onsole.log(edition)
     try {
       if (edition && account !== "") {
-        const res = await setDataContract.methods.gettypeToId(edition).call();
+        const tokenId = await setDataContract.gettypeToId(edition);
         //console.log(edition)
-        let tokenId = res;
         //console.log("확인",res)
-        const response = await caver.klay.sendTransaction({
-          from: account,
-          to: MINT_CONTRACT_ADDRESS,
-          gas: "3000000",
-          data: RandomJolamanContract.methods
-            .approve(SALE_CONTRACT_ADDRESS, tokenId)
-            .encodeABI(),
-        });
+        const response = await RandomJolamanContract.approve(SALE_CONTRACT_ADDRESS, tokenId);
+        // const response = await caver.klay.sendTransaction({
+        //   from: account,
+        //   to: MINT_CONTRACT_ADDRESS,
+        //   gas: "3000000",
+        //   data: RandomJolamanContract.methods
+        //     .approve(SALE_CONTRACT_ADDRESS, tokenId)
+        //     .encodeABI(),
+        // });
         // 판매 등록 함수 판매 등록시 두번쨰 함수
         console.log(response);
         if (response.status) {
           console.log("들어오나?");
-          console.log("계정확인", account);    
-            const res2 = await caver.klay.sendTransaction({
-              from: account,
-              to: SALE_CONTRACT_ADDRESS,
-              gas: "3000000",
-              data: SaleContract.methods
-                .SellJolamanToken(edition, price)
-                .encodeABI(),
-            });
+          // console.log("계정확인", account);    
+          const res2 = await SaleContract.SellJolamanToken(edition, price);
+          //   const res2 = await caver.klay.sendTransaction({
+          //     from: account,
+          //     to: SALE_CONTRACT_ADDRESS,
+          //     gas: "3000000",
+          //     data: SaleContract.methods
+          //       .SellJolamanToken(edition, price)
+          //       .encodeABI(),
+          //   });
             console.log(res2.status)
             if(res2.status) {
               dispatch({
