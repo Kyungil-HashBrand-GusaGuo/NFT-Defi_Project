7fa0b5dae3efbe9b5ebe1f7122859a0f9313de87
jaewoneeee <chou60400@naver.com>
2022-06-24 16:15:21 +0900

    ERROR : hangman game useEffect apply

--- frontend/src/pages/HangManGame.js
+++ frontend/src/pages/HangManGame.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import './HangManGame.css'
 import {Hangman} from '../components'
 
@@ -7,47 +7,57 @@ import { words } from '../components/GamePage/wordBank'
 
 const HangManGame = () => {
 
-    let selectedWord = words[Math.floor(Math.random() * words.length)];
-
-    const [correctLetters, setCorrectLetters] = useState([]);
+      const [correctLetters, setCorrectLetters] = useState([]);
       const [wrongLetters, setWrongLetters] = useState([]);
       const [playable, setPlayable] = useState(true);
       const [won, setWon] = useState(false);
+      const [selectedWord, setSelectedWord] = useState('')
     
       const playAgain = () => {
         setPlayable(true);
         setCorrectLetters([]);
         setWrongLetters([]);
-        selectedWord = words[Math.floor(Math.random() * words.length)];
+        //selectedWord = words[Math.floor(Math.random() * words.length)];
         setWon(false);
         const buttons = document.querySelectorAll("button");
         buttons.forEach((button) => {
           button.disabled = false; 
         });
+        let selectedRandomWord = words[Math.floor(Math.random() * words.length)];
+        setSelectedWord(selectedRandomWord)
       }
     
+      useEffect ( () => {
+        let selectedRandomWord = words[Math.floor(Math.random() * words.length)];
+        setSelectedWord(selectedRandomWord)
+      },[])
+
   return (
     <div className='HangManMainContainer'>
-    <div className='HangManContainer'>
-        <Hangman
-        wrongLetters={wrongLetters}
-        playable={playable}
-        playAgain={playAgain}
-        won={won}
-        word = {selectedWord}
-      />
-      <WordAndAlphabet
-        word={selectedWord}
-        wrongLetters={wrongLetters}
-        setWrongLetters={setWrongLetters}
-        correctLetters={correctLetters}
-        setCorrectLetters={setCorrectLetters}
-        setPlayable={setPlayable}
-        playable={playable}
-        setWon={setWon}
-        
-      />
-    </div>
+      {
+        selectedWord !== '' ?
+        <div className='HangManContainer'>
+            <Hangman
+            wrongLetters={wrongLetters}
+            playable={playable}
+            playAgain={playAgain}
+            won={won}
+            word = {selectedWord}
+          />
+          <WordAndAlphabet
+            word={selectedWord}
+            wrongLetters={wrongLetters}
+            setWrongLetters={setWrongLetters}
+            correctLetters={correctLetters}
+            setCorrectLetters={setCorrectLetters}
+            setPlayable={setPlayable}
+            playable={playable}
+            setWon={setWon}
+            
+          />
+        </div>
+        : null
+      }
     </div>
   )
 }
