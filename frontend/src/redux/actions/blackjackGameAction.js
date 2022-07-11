7c4f0d00da225cf28bd805ea4ac70b5433b8f2b1
taehhh8 <taehyun8534@gmail.com>
2022-07-11 11:03:03 +0900

    7/11
    7/11

--- frontend/src/redux/actions/blackjackGameAction.js
+++ frontend/src/redux/actions/blackjackGameAction.js
@@ -1,23 +1,42 @@
+import axios from "axios"; /* backend */
 import api from "../api"; /* contracts */
 
-function blackjackGameAct(title, bet) {
-    console.log(title)
-    console.log(bet)
+function blackjackGameAct(account, betPrice) {
+
     return async (dispatch) => {
         try {
-            // console.log("계정확인",account)
-            // console.log("total양 확인",total)
-            // const response = await api.post("/block/blackjackgamereward", {account, total});
+            console.log("계정확인",account)
+            console.log("배팅금액 확인",betPrice)
+            const response = await api.post("/block/blackjackwin", {account, betPrice});
+            // const response = await api.post("/block/blackjacklose", {account, betPrice});
 
-            // if(response.status) {
-            //     dispatch({
-            //         type: "CLEAR_BLACKJACK_GAME",
-            //         payload: { clearBlackJackGame: true}
-            //     })
-            //     if(total) {
-                    
-            //     }
-            // }
+            if(response.status) {
+                dispatch({
+                    type: "CLEAR_BLACKJACK_GAME",
+                    payload: { clearBlackJackGame: true}
+                })
+                
+                if(betPrice > 0) {
+                    dispatch({
+                        type: "GET_BLACKJACK_GAME_REWARD",
+                        payload: { blackjackRewardGZLT : 5 }
+                    })
+                } 
+
+                if (betPrice == 0) {
+                    dispatch({
+                        type: "GET_BLACKJACK_GAME_REWARD",
+                        payload: { blackjackRewardGZLT : 3}
+                    })
+                }
+
+                if ( betPrice < 0) {
+                    dispatch({
+                        type: "GET_BLACKJACK_GAME_REWARD",
+                        payload: { blackjackRewardGZLT : 2 }
+                    })
+                }
+            }
             
         } catch (error) {
             console.log(error);
