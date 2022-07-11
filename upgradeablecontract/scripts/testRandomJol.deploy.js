6254c4b167d6034b1951246081f4601a083de6ce
junghyunB <bjh940126@gmail.com>
2022-07-11 11:23:36 +0900

    V2 test

--- upgradeablecontract/scripts/testRandomJol.deploy.js
+++ upgradeablecontract/scripts/testRandomJol.deploy.js
@@ -5,7 +5,7 @@ async function main() {
 
     //TODO 배포 스크립트
     const metadataURI = "https://gateway.pinata.cloud/ipfs/QmaavyzfX6XzVNJx4zKCQVNDJWwQJx9xUC6gmDfddxvQ6p"
-    const setdataContract = "0x7b26320EA0adf2E3c4E081D2d19D8D78DCA6d63B"
+    const setdataContract = "0x10e844550E4364bAFe98Bb9Abd5f4978DfeB7f94"
     const RandomJolaman = await hre.ethers.getContractFactory("RandomJolaman");
     const ssu = await upgrades.deployProxy(RandomJolaman, [metadataURI, setdataContract], {initializer: "initialize"});
 
