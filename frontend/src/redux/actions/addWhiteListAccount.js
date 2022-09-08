2ed794ec1c9abd5cae23f1d0a7479bd96eb97dda
sieun95 <92782143+sieun95@users.noreply.github.com>
2022-09-08 12:02:26 +0900

    test

--- frontend/src/redux/actions/addWhiteListAccount.js
+++ frontend/src/redux/actions/addWhiteListAccount.js
@@ -1,24 +1,40 @@
-import { caver, RandomJolamanContract, MINT_CONTRACT_ADDRESS } from "../../caverConfig";
+import {
+  caver,
+  RandomJolamanContract,
+  MINT_CONTRACT_ADDRESS,
+} from "../../caverConfig";
+import { ethers } from "ethers";
+import { setDataContract } from "../../config";
+import { RandomJolamanContract } from "../../config";
 
 function addWhiteList(account, address) {
-  
-  // console.log("해당계정", account)  
+  // console.log("해당계정", account)
   // console.log("추가계정", address)
 
   return async (dispatch) => {
-        try {
-            const response = await caver.klay.sendTransaction({
-              from: account,
-              to: MINT_CONTRACT_ADDRESS,
-              gas: "3000000",
-              data: RandomJolamanContract.methods.addWhiteList(address).encodeABI(),
-            });
-            // console.log(response)
-            dispatch({type:"ADD_WHITELIST", payload : {addWhiteList : true}})
-          } catch(error) {
-            console.error(error);
-          }
+    try {
+      const response = await caver.klay.sendTransaction({
+        from: account,
+        to: MINT_CONTRACT_ADDRESS,
+        gas: "3000000",
+        data: RandomJolamanContract.methods.addWhiteList(address).encodeABI(),
+      });
+
+      const response2 = await setDataContract.signer.sendTransaction({
+        from: account,
+        to: RandomJolamanContract.MINT_CONTRACT_ADDRESS,
+        // value: ethers.utils.parseEther("0.2"),
+        gasPrice: "3000000",
+        // data: abiCoder.encode(["string"], ["asdfsdfasdf"]),
+        data: RandomJolamanContract.addWhiteList(address),
+      });
+
+      // console.log(response)
+      dispatch({ type: "ADD_WHITELIST", payload: { addWhiteList: true } });
+    } catch (error) {
+      console.error(error);
     }
+  };
 }
 
-export const addWhiteListAccount = {addWhiteList}
+export const addWhiteListAccount = { addWhiteList };
--- frontend/src/redux/actions/preMintingAction.js
+++ frontend/src/redux/actions/preMintingAction.js
@@ -1,37 +1,45 @@
-import { caver, RandomJolamanContract, MINT_CONTRACT_ADDRESS, setDataContract } from "../../caverConfig";
+import {
+  caver,
+  RandomJolamanContract,
+  MINT_CONTRACT_ADDRESS,
+  setDataContract,
+} from "../../caverConfig";
 import pinataApi from "../pinataApi";
 
 function preMintAction(account) {
+  // console.log(account)
 
-    // console.log(account)
+  return async (dispatch) => {
+    try {
+      const response = await caver.klay.sendTransaction({
+        from: account,
+        to: MINT_CONTRACT_ADDRESS,
+        value: caver.utils.convertToPeb(2, "KLAY"),
+        gas: "3000000",
+        data: RandomJolamanContract.methods.payandMint().encodeABI(),
+      });
+      // console.log("if문 전",response)
+      if (response.status) {
+        const response = await setDataContract.methods
+          .getTotalOwnedTokens(account)
+          .call();
+        let metaDataURI = response[response.length - 1];
+        const getMetaData = async () => {
+          const response = await pinataApi.get(`/${metaDataURI}.json`);
+          // console.log( "if문 안",response.data)
+          dispatch({
+            type: "GET_PRE_MINTDATA",
+            payload: { premintdata: response.data },
+          });
+        };
+        getMetaData();
+      }
 
-    return async (dispatch) => {
-        try {
-            const response = await caver.klay.sendTransaction({
-              from: account,
-              to: MINT_CONTRACT_ADDRESS,
-              value: caver.utils.convertToPeb(2, "KLAY"),
-              gas: "3000000",
-              data: RandomJolamanContract.methods.payandMint().encodeABI(),
-            })
-            // console.log("if문 전",response)
-            if(response.status) {
-              const response = await setDataContract.methods.getTotalOwnedTokens(account).call()
-              let metaDataURI = response[response.length-1]; 
-              const getMetaData = async() => {
-                const response = await pinataApi.get(`/${metaDataURI}.json`);
-                // console.log( "if문 안",response.data)
-                dispatch({type : "GET_PRE_MINTDATA", payload : {premintdata : response.data}})         
-              }
-              getMetaData();
-            }
-
-            
-
-          } catch (error){
-            console.error(error);
-          }
+      A``;
+    } catch (error) {
+      console.error(error);
     }
+  };
 }
 
-export const preMintingAction = {preMintAction}
+export const preMintingAction = { preMintAction };
